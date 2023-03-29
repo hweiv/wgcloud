f275fc52064baffb538a492f112c0cb3cf4a4dd5
huang.wei <huang.wei@i-jimi.com.cn>
2023-03-29 14:50:48 +0800

    111

--- wgcloud-server/src/main/java/com/wgcloud/config/Server.java
+++ wgcloud-server/src/main/java/com/wgcloud/config/Server.java
@@ -10,29 +10,33 @@ import java.net.*;
 @Configuration
 @Slf4j
 public class Server {
-//    public static void main(String[] args) throws IOException {
-//        ServerSocket server = new ServerSocket(9888);
-//        Socket socket = server.accept();
-//        InputStream in = socket.getInputStream();
-//        DataInputStream dis = new DataInputStream(in);
-//        while (true) {
-//            String message = dis.readUTF();
-//            System.out.println(message);
-//        }
-//    }
-
-    public static void init() {
-        try {
-            ServerSocket server = new ServerSocket(9888);
-            Socket socket = server.accept();
-            InputStream in = socket.getInputStream();
-            DataInputStream dis = new DataInputStream(in);
-            while (true) {
-                String message = dis.readUTF();
-                log.info("the serverListener get msg:{}", message);
-            }
-        } catch (IOException e) {
-            log.error("the Cilent.class-check: error:{}", e);
+    public static void main(String[] args) throws IOException {
+        ServerSocket server = new ServerSocket(9888);
+        Socket socket = server.accept();
+        InputStream in = socket.getInputStream();
+        DataInputStream dis = new DataInputStream(in);
+        // 输出文件全路径
+        String filePath = "F:\\Listener_log.txt";
+        while (true) {
+            String message = dis.readUTF();
+            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(filePath));
+            bufferedWriter.write(message);
+            System.out.println(message);
         }
     }
+
+//    public static void init() {
+//        try {
+//            ServerSocket server = new ServerSocket(9888);
+//            Socket socket = server.accept();
+//            InputStream in = socket.getInputStream();
+//            DataInputStream dis = new DataInputStream(in);
+//            while (true) {
+//                String message = dis.readUTF();
+//                log.info("the serverListener get msg:{}", message);
+//            }
+//        } catch (IOException e) {
+//            log.error("the Cilent.class-check: error:{}", e);
+//        }
+//    }
 }
